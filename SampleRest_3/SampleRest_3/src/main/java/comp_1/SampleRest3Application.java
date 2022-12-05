57211db52d4998d3acfe134f95f37e8ea27f5adb
Shridhara Chaube <chaubey.shridhara@gmail.com>
2022-12-05 16:10:36 +0530

    Shri

---
+++ SampleRest_3/SampleRest_3/src/main/java/comp_1/SampleRest3Application.java
@@ -0,0 +1,13 @@
+package comp_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRest3Application.class, args);
+	}
+
+}
---
+++ SampleRest_3/SampleRest_3/src/test/java/comp_1/SampleRest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package comp_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
